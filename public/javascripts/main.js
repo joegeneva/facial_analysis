08940eda8ea4c25d4713ef9d9988af8951fd4544
joegeneva <joepgeneva@gmail.com>
2016-09-14 14:19:30 -0400

    cleaning Jquery

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -14,39 +14,18 @@ var controlLoop = new function(){
 };
 
 
-function chartdata(){return {emotion:selectedVal,posarray: JSON.stringify(positions)}}
-var positionloop;
-var senddata = function(){
 
-//var chartdata = ;
-  $.post("/train",chartdata(),function( data ) {
-  console.log( data);
-  console.log("success!");
-  }, "json")
-  .fail(function(a,b,c) {
-    console.log(b);
-    console.log(c);
-  })
+function chartBundle(){return {emotion:selectedVal,posarray: JSON.stringify(positions)}}
+var chartDataArray = [];
+function chartData(){return {data:chartDataArray}}
+
+var collectdata = function(){
+  chartDataArray.push(chartBundle());
 }
 
-$("#start").click(function(){ controlLoop.run(senddata); });
+$("#start").click(function(){ controlLoop.run(collectdata); });
 
 $("#stop").click(function(){ controlLoop.check = true; });
-
-//$( "#start" ).click(function() {
-  //data is gotten back from server
-  //var timeup = 0;
-  //positionloop = window.setInterval(function(){
-    //timeup+=1;
-    //if (timeup == 1){
-      //window.clearInterval(positionloop);
-    //}
-    
-  //},500)
-//
-//$( "#stop" ).click(function() {
-    //window.clearInterval(positionloop);
-//});
 
 //get value for chart
 var selectedVal = 0;
@@ -63,81 +42,45 @@ $( "input[name=emotion]" ).change(function() {
 
 $( "#activate" ).click(function() {
   //data is gotten back from server
-  //positionloop = window.setInterval(function(){
-    chartdata = {emotion:selectedVal,posarray: JSON.stringify(positions)};
-    $.post("/activate",chartdata,function( data ) {
+  $.post("/activate",chartdata(),function( data ) {
     console.log( data);
     data.forEach(function(currentValue,index){
       chart.options.data[0].dataPoints[index].y = currentValue;
-
       //chart.options.data[0].dataPoints[Math.round(data[0])].y = 1;
     });
     chart.render();
     }, "json")
-    .fail(function(a,b,c) {
-      console.log(b);
-      console.log(c);
-    })
-  //},1000);
 });
 
 $( "#save" ).click(function() {
   //data is gotten back from server
-  var timeup = 0;
-  positionloop = window.setInterval(function(){
-    timeup+=1;
-    console.log(timeup);
-    if (timeup == 5){
-      window.clearInterval(positionloop);
-    }
-  chartdata = {emotion:selectedVal,posarray: JSON.stringify(positions)};
-    $.post("/save",chartdata,function(data) {
+    $.post("/save",chartdata(),function(data) {
       //console.log(data);
     }, "json")
-    .fail(function(a,b,c) {
-      console.log(b);
-      console.log(c);
-    })
-  },1000);
 });
 
 $( "#load" ).click(function() {
   //data is gotten back from server
-  //positionloop = window.setInterval(function(){
     $.post("/load",function( data ) {
       console.log(data);
     }, "json")
-    .fail(function(a,b,c) {
-      console.log(b);
-      console.log(c);
-    })
-  //},1000);
 });
 
 $( "#delete" ).click(function() {
-  //data is gotten back from server
-  //positionloop = window.setInterval(function(){
     $.post("/delete",function( data ) {
       console.log(data);
     }, "json")
-    .fail(function(a,b,c) {
-      console.log(b);
-      console.log(c);
-    })
-  //},1000);
 });
 
 
 $( "#test" ).click(function() {
-    chartdata = {emotion:selectedVal,posarray: JSON.stringify(positions)};
-    $.post("/test",chartdata,function( data ) {
+    $.post("/test",chartdata(),function( data ) {
     console.log( data);
   })
 });
 
 $( "#newnn" ).click(function() {
-    chartdata = {emotion:selectedVal,posarray: JSON.stringify(positions)};
-    $.post("/newnn",chartdata,function( data ) {
+    $.post("/newnn",chartdata(),function( data ) {
     console.log( data);
   })
 });
